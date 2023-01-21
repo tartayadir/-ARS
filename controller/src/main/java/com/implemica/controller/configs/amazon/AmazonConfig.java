631fca36c2ab98a40bc3aacecd3ebe442df204ce
Yaroslav B <yaroslav.b@implemica.com>
2023-01-21 17:49:30 +0300

    fix issue with s3 bucket

--- controller/src/main/java/com/implemica/controller/configs/amazon/AmazonConfig.java
+++ controller/src/main/java/com/implemica/controller/configs/amazon/AmazonConfig.java
@@ -1,5 +1,8 @@
 package com.implemica.controller.configs.amazon;
 
+import com.amazonaws.auth.AWSCredentialsProviderChain;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +16,15 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
 
-        return AmazonS3ClientBuilder.defaultClient();
+        AWSCredentialsProviderChain providerChain = new AWSCredentialsProviderChain(
+                InstanceProfileCredentialsProvider.getInstance(),
+                new ProfileCredentialsProvider()
+        );
+
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(providerChain)
+                .build();
+//        return AmazonS3ClientBuilder.defaultClient();
     }
 
 }
